90767bfa89fc3582b6491feb02acf23521496a97
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-10-29 16:36:37 +0100

    Config

---
+++ models/config.py
@@ -0,0 +1,76 @@
+from .differential import DifferentialViT
+from .classic import ClassicViT
+from typing import Any
+
+models: dict[str, Any] = {
+    "classic": {
+        "10M": ClassicViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=384,
+            depth=12,
+            num_heads=8,
+            mlp_dim=1024,
+            in_channels=3,
+            dropout=0.1,
+        ),
+        "20M": ClassicViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=512,
+            depth=14,
+            num_heads=8,
+            mlp_dim=1408,
+            in_channels=3,
+            dropout=0.1,
+        ),
+        "30M": ClassicViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=640,
+            depth=16,
+            num_heads=8,
+            mlp_dim=1536,
+            in_channels=3,
+            dropout=0.1,
+        ),
+    },
+    "differential": {
+        "10M": DifferentialViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=384,
+            depth=12,
+            num_heads=8,
+            mlp_dim=1024,
+            in_channels=3,
+            dropout=0.1,
+        ),
+        "20M": DifferentialViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=512,
+            depth=14,
+            num_heads=8,
+            mlp_dim=1408,
+            in_channels=3,
+            dropout=0.1,
+        ),
+        "30M": DifferentialViT(
+            img_size=64,
+            patch_size=8,
+            num_classes=200,
+            embed_dim=640,
+            depth=16,
+            num_heads=8,
+            mlp_dim=1536,
+            in_channels=3,
+            dropout=0.1,
+        ),
+    },
+}
